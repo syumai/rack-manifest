5b10cc9ca3023d87fef7a3ae39216ff8ee032d0d
syumai <syumai@gmail.com>
2016-07-09 12:31:07 +0900

    Added Last-Modified and 304 response

--- lib/rack/manifest.rb
+++ lib/rack/manifest.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'erb'
 require 'action_view/helpers/asset_url_helper'
+require 'rack/request'
 require 'rack/manifest/version'
 require 'rack/manifest/rails' if defined?(Rails::Railtie)
 require 'rack/manifest/sprockets' if defined?(Sprockets) && defined?(Rails)
@@ -22,16 +23,31 @@ class Rack::Manifest
 
   def call(env)
     if env['PATH_INFO'] == '/manifest.json'
+      headers = {}
+      if env.has_key?('HTTP_IF_MODIFIED_SINCE')
+        fetched_date = env['HTTP_IF_MODIFIED_SINCE']
+        return [304, headers, []] if get_modified_time(FILE_PATH) == fetched_date
+      end
       manifest = load_yaml(FILE_PATH)
       json = JSON.generate(manifest)
       [
         200,
-        {'Content-Type' => 'application/json'},
+        headers.merge({
+          'Content-Type' => 'application/json',
+          'Last-Modified' => get_modified_time(FILE_PATH),
+          'Content-Length' => json.length.to_s
+        }),
         [json]
       ]
     else
       @app.call(env)
     end
+  end
+
+  private
+  def get_modified_time(path)
+    time = File.mtime(path)
+    time.strftime('%a, %d %b %Y %H:%M:%S GMT')
   end
 end
 
